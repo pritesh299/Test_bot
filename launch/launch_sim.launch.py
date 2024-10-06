946e535f9cb824a642aa1c215d245c8da37657f1
Pritesh Jogdhankar <129420569+pritesh299@users.noreply.github.com>
2024-10-06 00:34:15 +0000

    updated from scratch

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,8 +1,8 @@
 import os
 
-
-
 from ament_index_python.packages import get_package_share_directory
+
+
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
@@ -36,25 +36,23 @@ def generate_launch_description():
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
+   
+    diff_drive_spwaner = Node(
+                        package='controller_manager', 
+                        executable='spawner',
+                        arguments=['diff_cont'])
     
-    diff_drive_spawner = Node(
-        package='controller_manager', 
-        executable='spawner',
-        arguments=['diff_cont']
-    )
-
-    joint_broad_spawner = Node(
-        package='controller_manager', 
-        executable='spawner',
-        arguments=['joint_broad']
-    )
+    joint_broad_spwaner = Node(
+                        package='controller_manager', 
+                        executable='spawner',
+                        arguments=['joint_broad'])
+   
 
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner
-
+        diff_drive_spwaner,
+        joint_broad_spwaner
     ])
