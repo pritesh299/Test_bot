ae43e1318edae49defc68302907b3e6ade368880
Pritesh Jogdhankar <129420569+pritesh299@users.noreply.github.com>
2024-11-17 05:37:03 +0530

    fixed controller issue

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -14,49 +14,54 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='Test_bot' #<--- CHANGE ME
-
-    gazebo_params_path = os.path.join(
-                  get_package_share_directory(package_name),'config','gazebo_params.yaml')
-    
+    package_name='Test_bot' 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_path }.items()
-
              )
+    
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
+                                   '-entity', 'test_bot'],
                         output='screen')
-   
-    diff_drive_spwaner = Node(
-                        package='controller_manager', 
-                        executable='spawner',
-                        arguments=['diff_cont'])
-    
-    joint_broad_spwaner = Node(
-                        package='controller_manager', 
-                        executable='spawner',
-                        arguments=['joint_broad'])
-   
+
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
+
+
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
+        twist_mux,
         spawn_entity,
-        diff_drive_spwaner,
-        joint_broad_spwaner
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
